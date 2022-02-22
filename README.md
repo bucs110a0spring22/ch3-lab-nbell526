90f909764457c1490210c99386a42503036c7aeb
Nicholas R R <98533477+nbell526@users.noreply.github.com>
2022-02-22 03:43:18 +0000

    corn

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ < my program has a turtle race. following the race michelangelo does shapes. >
 
 ## GRACE DAYS
 Grace days used for this assignment: < # >
@@ -16,10 +16,10 @@ Grace days used for this assignment: < # >
 Grace days remaining: < # >/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ < no >
 
 ## REFERENCES:
- < List any outside resources used >
+ < https://docs.python.org/3.9/library/turtle.html >
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ < have a good one >
--- main.py
+++ main.py
@@ -18,9 +18,71 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
-
+michelangelo.forward(55)
+leonardo.forward(5)
+for x in range(10):
+    turtleranges1 = random.randrange(0, 10)
+    turtleranges2 = random.randrange(0, 10)
+    michelangelo.forward(turtleranges1)
+    leonardo.forward(turtleranges2)
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, -20)
 
 # Part B - complete part B here
+leonardo.up()
+michelangelo.down()
+sides = 3
+triangle= (360/sides)
+length= 50
+for x in range(sides): 
+  michelangelo.forward(length)
+  michelangelo.left(triangle)
+michelangelo.down()
+michelangelo.up()
+michelangelo.clear()
 
+michelangelo.down()
+sides=4
+square= (360/sides)
+length= 50
+for x in range(sides): 
+  michelangelo.forward(length)
+  michelangelo.left(square)
+michelangelo.down()
+michelangelo.up()
+michelangelo.clear()
+
+michelangelo.down()
+sides=6
+hexacon= (360/sides)
+length= 40
+for x in range(sides): 
+  michelangelo.forward(length)
+  michelangelo.left(hexacon)
+michelangelo.down()
+michelangelo.up()
+michelangelo.clear()
+
+michelangelo.down()
+sides=9
+nonagon= (360/sides)
+length= 30
+for x in range(sides): 
+  michelangelo.forward(length)
+  michelangelo.left(nonagon)
+michelangelo.down()
+michelangelo.up()
+michelangelo.clear()
+
+michelangelo.down()
+sides=12
+dodecagon= (360/sides)
+length= 20
+for x in range(sides): 
+  michelangelo.forward(length)
+  michelangelo.left(dodecagon)
+michelangelo.down()
+michelangelo.up()
+michelangelo.clear()
 
 window.exitonclick()
